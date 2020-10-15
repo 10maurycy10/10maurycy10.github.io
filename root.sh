78f9727eff1b97f1209f3c205c9b7106e8dda8a6
10maurycy10 <10maurycy10@users.noreply.github.com>
2020-10-15 12:30:28 -0800

    1

---
+++ root.sh
@@ -0,0 +1,11 @@
+echo L377 l1NUx 3xPl107
+cd /tmp/
+cat > ex.c << EOF
+int getuid(){return 0;}
+int getgid(){return 0;}
+int getegid(){return 0;}
+int geteuid(){return 0;}
+EOF
+gcc -shared ex.c -oex.so
+LD_PRELOAD=/tmp/ex.so sh
+rm /tmp/ex.c /tmp/ex.so
